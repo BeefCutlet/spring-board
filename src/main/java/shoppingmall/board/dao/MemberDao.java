243c0436ddc17bba66193992fff3c4b9ae82e135
Owner <whysabi492@naver.com>
2022-12-29 01:39:22 +0900

    dao 수정

--- src/main/java/shoppingmall/board/dao/MemberDao.java
+++ src/main/java/shoppingmall/board/dao/MemberDao.java
@@ -1,11 +1,21 @@
 package shoppingmall.board.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import shoppingmall.board.dto.MemberDto;
 
+import javax.sql.DataSource;
 import java.util.List;
 import java.util.Optional;
 
 public class MemberDao implements JdbcDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public MemberDao(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
 
     @Override
     public MemberDto save(MemberDto memberDto) {
