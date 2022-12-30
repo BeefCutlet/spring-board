004004f582e7f0b747119b71744996283991a7e8
Owner <whysabi492@naver.com>
2022-12-31 00:50:15 +0900

    DAO 메소드 구현 - save - findById - findByName - findAll

--- src/main/java/shoppingmall/board/dao/MemberDao.java
+++ src/main/java/shoppingmall/board/dao/MemberDao.java
@@ -2,9 +2,12 @@ package shoppingmall.board.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import shoppingmall.board.dto.MemberDto;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,21 +22,61 @@ public class MemberDao implements JdbcDao {
 
     @Override
     public MemberDto save(MemberDto memberDto) {
-        return null;
+        jdbcTemplate.update("INSERT INTO member VALUES(?, ?, ?, ?, ?, ?)",
+                memberDto.getId(),
+                memberDto.getPassword(),
+                memberDto.getEmail(),
+                memberDto.getJoinDate(),
+                memberDto.getLastLogin(),
+                memberDto.getStatus());
+
+        return memberDto;
     }
 
     @Override
     public Optional<MemberDto> findById(String id) {
-        return Optional.empty();
+        List<MemberDto> result = jdbcTemplate.query(
+                "SELECT " +
+                        "id, nickname, password, email, join_date, last_login, status " +
+                        "FROM member" +
+                        "JOIN nick ON member.id = nick.nick_id" +
+                        "WHERE id=?", memberRowMapper(), id);
+        return result.stream().findAny();
     }
 
     @Override
-    public Optional<MemberDto> findByName(String name) {
-        return Optional.empty();
+    public Optional<MemberDto> findByName(String nickname) {
+        List<MemberDto> result = jdbcTemplate.query(
+                "SELECT " +
+                        "id, nickname, password, email, join_date, last_login, status " +
+                        "FROM member" +
+                        "JOIN nick ON member.id = nick.nick_id" +
+                        "WHERE nickname=?", memberRowMapper(), nickname);
+        return result.stream().findAny();
     }
 
     @Override
     public List<MemberDto> findAll() {
-        return null;
+        List<MemberDto> memberList = jdbcTemplate.query(
+                "SELECT " +
+                        "id, nickname, password, email, join_date, last_login, status " +
+                        "FROM member" +
+                        "JOIN nick ON member.id = nick.nick_id", memberRowMapper());
+
+        return memberList;
+    }
+
+    private RowMapper<MemberDto> memberRowMapper() {
+        return (rs, rowNum) -> {
+            MemberDto member = new MemberDto();
+            member.setId(rs.getString("id"));
+            member.setNickname(rs.getString("nickname"));
+            member.setPassword(rs.getString("password"));
+            member.setEmail(rs.getString("email"));
+            member.setJoinDate(rs.getString("join_date"));
+            member.setLastLogin(rs.getString("last_login"));
+            member.setStatus(rs.getInt("status"));
+            return member;
+        };
     }
 }
