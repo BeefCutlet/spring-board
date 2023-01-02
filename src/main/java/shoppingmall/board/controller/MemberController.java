700e6d375b3a60e45911c38a0102c5c3c80a3e20
Owner <whysabi492@naver.com>
2023-01-02 23:57:17 +0900

    MemberController 추가 - 로그인, 회원가입 관련 메소드 형태 정의

---
+++ src/main/java/shoppingmall/board/controller/MemberController.java
@@ -0,0 +1,31 @@
+package shoppingmall.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller("/member")
+public class MemberController {
+
+    @GetMapping("/join")
+    public String joinMemberForm() {
+
+        return "";
+    }
+
+    @PostMapping("/join")
+    public String joinMember() {
+        return "";
+    }
+
+    @GetMapping("/login")
+    public String loginMemberForm() {
+
+        return "";
+    }
+
+    @PostMapping("/login")
+    public String loginMember() {
+        return "";
+    }
+}
